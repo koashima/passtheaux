2d788d7af168efa9e0037c566c898fad53391a8a
koashima <48111257+koashima@users.noreply.github.com>
2020-12-03 21:08:20 -0800

    refactors Home and App components and adds basic layout of Home

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
+import RoomJoin from './RoomJoin';
+import CreateRoom from './CreateRoom';
+import Room from './Room';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
 import Home from './Home';
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/join" component={RoomJoin} />
+          <Route path="/create" component={CreateRoom} />
+          <Route path="/room/:roomCode" component={Room} />
+        </Switch>
+      </Router>
     </>
   );
 };
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import RoomJoin from './RoomJoin';
-import CreateRoom from './CreateRoom';
-import Room from './Room';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core';
 
 const Home = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" />
-        <Route path="/join" component={RoomJoin} />
-        <Route path="/create" component={CreateRoom} />
-        <Route path="/room/:roomCode" component={Room} />
-      </Switch>
-    </Router>
+    <div>
+      <Grid
+        container
+        spacing={3}
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item xs={12}>
+          <Typography variant="h2" compact="h2">
+            PASSTHEAUX
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              JOIN ROOM
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              CREATE ROOM
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
