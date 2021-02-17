cf831ea36ca9bb85ccbc60a0fcb6375cc79b3a72
koashima <48111257+koashima@users.noreply.github.com>
2021-02-16 22:21:16 -0800

    gets current song. (just rewrote the code don't know what was different)

--- spotifyAPI/utils.py
+++ spotifyAPI/utils.py
@@ -1,12 +1,14 @@
 from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta
-from requests import post
+from requests import post, put, get
 
 import environ
 
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env()
+
+BASE_URL = "https://api.spotify.com/v1/me/"
 
 
 def get_user_tokens(session_key):
@@ -29,7 +31,7 @@ def update_or_create_user_tokens(
         tokens.expires_in = expires_in
         tokens.token_type = token_type
         tokens.save(
-            update_fields=["access_token", "refresh_token", "expires_in" "token_type"]
+            update_fields=["access_token", "refresh_token", "expires_in", "token_type"]
         )
     else:
         tokens = SpotifyToken(
@@ -74,3 +76,22 @@ def refresh_spotify_token(session_key):
     update_or_create_user_tokens(
         session_key, access_token, token_type, expires_in, refresh_token
     )
+
+
+def execute_spotify_api_request(session_key, endpoint, post_=False, put_=False):
+    tokens = get_user_tokens(session_key)
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + tokens.access_token,
+    }
+
+    if post_:
+        post(BASE_URL + endpoint, headers=headers)
+    if put_:
+        put(BASE_URL + endpoint, headers=headers)
+
+    response = get(BASE_URL + endpoint, {}, headers=headers)
+    try:
+        return response.json()
+    except:
+        return {"Error": "Issue with request"}
