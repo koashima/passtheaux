0a68834eb029ed184e3f07ea7b73584fe8feb70f
koashima <48111257+koashima@users.noreply.github.com>
2021-02-23 15:33:30 -0800

    adds Procfile, gunicorn, django-on-heroku setup

--- passtheaux/settings.py
+++ passtheaux/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
-
+import django_heroku
 env = environ.Env(
     DEBUG=(bool, False)
 )
@@ -130,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+django_on_heroku.settings(locals())
